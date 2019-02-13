b836df57a9b7ddacaad35d70973435d62f6242eb
mattmcmahon <matt.mcmahon424@gmail.com>
2019-02-13 16:17:27 -0700

    starting ipscan and myip scripts

---
+++ ipscan.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+## grabs private ip address and cuts off host 
+addr_range="`ipconfig getifaddr en0| cut -d . -f 1-3`.*"
+
+echo $addr_range
+
+ipaddr=`ifconfig en0 | awk '$1=="inet" {print $2}'`
+
+
+while [ "$option" != "quit" ]; do
+
+	echo "Enter option (ipcalc, nmap, quit):"
+	read option
+
+	if [ "$option" == "ipcalc" ];
+
+		then
+		ipcalc $ipaddr;
+
+	elif [ "$option" == "nmap" ];
+	
+		then
+		echo "nmap options (-O,-sn, or both?):"
+
+		read nmOpt
+		
+		if [ "$nmOpt" == "-O" ];
+
+			then
+			nmap -O $addr_range;
+		
+		elif [ "$nmOpt" == "-sn" ];
+			then
+			nmap -sn $addr_range;
+
+		elif [ "$nmOpt" == "both" ];
+			then
+			nmap -Osn $addr_range;
+		
+		fi
+
+	fi
+done
