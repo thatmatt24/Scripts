b7892f72b98ae9a148ec55e50e1055c4544cca64
mattmcmahon <matt.mcmahon424@gmail.com>
2019-02-13 16:23:38 -0700

    myip initial

--- myip.sh
+++ myip.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
+pub_ip="ipconfig getifaddr en0"
+
+priv_ip1="dig +short myip.opendns.com @resolver1.opendns.com"
+
+priv_ip2="wget http://ipecho.net/plain -O - -q ; echo"
+
+priv_ip3="curl ipecho.net/plain; echo"
+
+
+if [ "$pub_ip" == "$priv_ip1" ];
+
+	then
+	echo $priv_ip2;
+
+fi 
+
+## make this check for each and return whichever != pub_ip. If none do, echo "note" and find out why (UDP vs TCP) in order to 
+### investigate that network further
+
+
 
