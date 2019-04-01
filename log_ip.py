5e5f948c0e6cc6c64df572db1b857add37e89fdd
mattmcmahon <matt.mcmahon424@gmail.com>
2019-04-01 16:04:39 -0600

    added SSID and cleanup

--- log_ip.py
+++ log_ip.py
@@ -12,37 +12,67 @@ import re
 def printer(list):
     file = open('/Users/mattmcmahon/Desktop/Scripts/ip-log1.nosync', 'a')
     
-    for i in list:
-
-        if ("----" in i):
-            i = '{:^17}'.format(i)
-            file.write('|' + i)
-        elif (i == list[len(list) - 1]):
-            i = '{:^17}'.format(i)
-            file.write('|' + i + '|')
-        elif ( i == socket.gethostname() or i == "sock_hostname"):
-            i = '{:^25}'.format(i)
-            file.write('|' + i)
+    for i,el in enumerate(list):
+        if (i == 0 or i == 9):
+            el = el[0:14]
+            el = '{:^14}'.format(el)
+            file.write('|' + el)
+        elif (i == 1):
+            el = el[0:11]
+            el = '{:^11}'.format(el)
+            file.write('|' + el)
+        elif (i == 8):
+            el = el[0:25]
+            el = '{:^25}'.format(el)
+            file.write('|' + el)
+        elif (i == 10):
+            el = el[0:16]
+            el = '{:^16}'.format(el)
+            file.write('|' + el)
+        elif (i == 11):
+            el = el[0:18]
+            if ("----" not in el):
+                el = '{:^17}'.format(el)
+                file.write('|' + el + "|")
+            else:
+                el = '{:^18}'.format(el)
+                file.write('|' + el)
         else:
-            i = '{:^17}'.format(i)
-            file.write('|' + i)
+            el = el[0:17]
+            el = '{:^17}'.format(el)
+            file.write('|' + el)
 
     file.write("\n")
     file.close()
 
 
 # example for formating:
-title = ['date', 'time', 'ext: urllib', 'ext: curl', 'ext: dig', 'ext: upnpc', 'internal', 'sock_hostname', 'latitude', 'longitude', 'city']
-lines = ['-----------------','-----------------','-----------------','-----------------','-----------------','-----------------','-----------------','-------------------------','-----------------','-----------------','-----------------|']
+title = ['date', 'time', 'SSID','ext: urllib', 'ext: curl', 'ext: dig', 'ext: upnpc', 'internal', 'sock_hostname', 'latitude', 'longitude', 'city']
+lines = ['--------------', '-----------', '-----------------', '-----------------', '-----------------', '-----------------', '-----------------', '-----------------', '-------------------------', '--------------', '----------------', '-----------------|']
+dt = tt = urlib = cur = dig = pnp = ipconfig = sock_name = lat = lon = city = "test"
+ssid = "-------------------"
+
 # printer(lines)
 # printer(title)
-printer(lines)
+# printer(lines)
 
-dt = datetime.datetime.now().strftime("%b %d %Y")
+dt = datetime.datetime.now().strftime("%b.%d.%Y")
 tt = datetime.datetime.now().strftime("%H:%M:%S")
 
 try:
-    ipconfig = socket.gethostbyname(socket.gethostname())
+    ssid = Popen(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-I'], stdout=PIPE, stderr=PIPE)
+    ssid, err = ssid.communicate()
+    ssid = ssid.decode('utf-8')
+    for i,el in enumerate(ssid.split()):
+        if(el == "SSID:"):
+            ssid = (ssid.split()[i + 1])
+except:
+    ssid = "Error"
+    print(ssid)
+
+try:
+    sock_name = socket.gethostname()
+    ipconfig = socket.gethostbyname(sock_name)
 except:
     ipconfig = Popen(['ipconfig', 'getifaddr', 'en0'], stdout=PIPE, stderr=PIPE)
     ipconfig, err = ipconfig.communicate()
@@ -86,9 +116,7 @@ except:
     pnp = "Error"
 
 
-lname = [dt, tt, urlib, cur, dig, pnp, ipconfig, socket.gethostname(), lat, lon, city]
+lname = [dt, tt, ssid,  urlib, cur, dig, pnp, ipconfig, sock_name, lat, lon, city]
 
 printer(lname)
 printer(lines)
-
-
