7d641aa507a782afa26a5eb302c57f106de109c1
mattmcmahon <matt.mcmahon424@gmail.com>
2019-04-11 11:33:13 -0600

    Fixed SSID name cutoff

--- log_ip.py
+++ log_ip.py
@@ -27,35 +27,43 @@ def printer(list):
             el = el[0:14]
             el = '{:^14}'.format(el)
             file.write('|' + el)
+            # print(i, " index ", el, " element ")
         elif (i == 1):
             el = el[0:11]
             el = '{:^11}'.format(el)
             file.write('|' + el)
+            # print(i, " index ", el, " element ")
         elif (i == 8):
             el = el[0:25]
             el = '{:^25}'.format(el)
             file.write('|' + el)
+            # print(i, " index ", el, " element ")
         elif (i == 10):
             el = el[0:16]
             el = '{:^16}'.format(el)
             file.write('|' + el)
+            # print(i, " index ", el, " element ")
         elif (i == 11):
             el = el[0:18]
             if ("----" not in el):
                 el = el[0:17]
                 el = '{:^17}'.format(el)
                 file.write('|' + el + "|")
+                # print(i, " index ", el, " element ")
             else:
                 el = '{:^18}'.format(el)
                 file.write('|' + el)
+                # print(i, " index ", el, " element ")
         elif (i == 2):
-            el = el[0:18]
+            el = el[0:17]
             el = '{:^17}'.format(el)
             file.write('|' + el)
+            # print(i, " index ", el, " element ")
         else:
             el = el[0:17]
             el = '{:^17}'.format(el)
             file.write('|' + el)
+            # print(i, " index ", el, " element ")
 
     file.write("\n")
     file.close()
@@ -77,9 +85,7 @@ try:
     ssid = Popen(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-I'], stdout=PIPE, stderr=PIPE)
     ssid, err = ssid.communicate()
     ssid = ssid.decode('utf-8')
-    for i,el in enumerate(ssid.split()):
-        if(el == "SSID:"):
-            ssid = (ssid.split()[i + 1])
+    ssid = ssid.split("SSID:")[2].split("\n")[0]
 except:
     ssid = "Error"
     print(ssid)
@@ -93,45 +99,45 @@ except:
     ipconfig = ipconfig.decode('utf-8').strip()
     print("socket error")
 
-try:
-    urlib = urllib.request.urlopen('http://ipecho.net/plain').read().decode('utf-8')
-except:
-    urlib = "Error"
-    print("urlib error")
+# try:
+#     urlib = urllib.request.urlopen('http://ipecho.net/plain').read().decode('utf-8')
+# except:
+#     urlib = "Error"
+#     print("urlib error")
 
-try:
-    cur = Popen(['curl', '-s', 'https://freegeoip.app/json/' , 'localhost'], stdout=PIPE, stderr=PIPE)
-    cur, err = cur.communicate()
-    cur = json.loads(cur)
-    lat = str(cur["latitude"])
-    lon = str(cur["longitude"])
-    city = cur["city"]
-    cur = cur["ip"]
-except:
-    lat = lon = city = cur = "Error"
-    print("curl error")
+# try:
+#     cur = Popen(['curl', '-s', 'https://freegeoip.app/json/' , 'localhost'], stdout=PIPE, stderr=PIPE)
+#     cur, err = cur.communicate()
+#     cur = json.loads(cur)
+#     lat = str(cur["latitude"])
+#     lon = str(cur["longitude"])
+#     city = cur["city"]
+#     cur = cur["ip"]
+# except:
+#     lat = lon = city = cur = "Error"
+#     print("curl error")
 
-try:
-    dig = Popen(['dig', '+short', 'myip.opendns.com', '@resolver1.opendns.com'], stdout=PIPE, stderr=PIPE)
-    dig, err = dig.communicate()
-    dig = dig.decode('utf-8').strip()
-except:
-    dig = "Error"
-    print("dig error")
+# try:
+#     dig = Popen(['dig', '+short', 'myip.opendns.com', '@resolver1.opendns.com'], stdout=PIPE, stderr=PIPE)
+#     dig, err = dig.communicate()
+#     dig = dig.decode('utf-8').strip()
+# except:
+#     dig = "Error"
+#     print("dig error")
 
-try:
-    pnp = Popen(['upnpc', '-s'], stdout=PIPE, stderr=PIPE)
-    pnp, err = pnp.communicate()
-    pnp = pnp.decode('utf-8')
-    pnp = pnp.split("ExternalIPAddress")[1].split(" = ")[1].split("\n")[0]
-    # ext_ip = pnp.index("ExternalIPAddress")
-    # pnp = pnp[(ext_ip + 20) : (ext_ip + 32)]
-    # pnp = re.match('([0-9]{1,3}\.){3}[0-9]{1,3}', pnp)
-    # pnp = pnp.group(0)
+# try:
+#     pnp = Popen(['upnpc', '-s'], stdout=PIPE, stderr=PIPE)
+#     pnp, err = pnp.communicate()
+#     pnp = pnp.decode('utf-8')
+#     pnp = pnp.split("ExternalIPAddress")[1].split(" = ")[1].split("\n")[0]
+#     # ext_ip = pnp.index("ExternalIPAddress")
+#     # pnp = pnp[(ext_ip + 20) : (ext_ip + 32)]
+#     # pnp = re.match('([0-9]{1,3}\.){3}[0-9]{1,3}', pnp)
+#     # pnp = pnp.group(0)
 
-except:
-    print('upnpc error')
-    pnp = "Error"
+# except:
+#     print('upnpc error')
+#     pnp = "Error"
 
 
 
